d45761395a69e74fb428ec2cedaa5299ede5bfd2
Kinshuk Bairagi <me@kinshuk.in>
2016-02-08 19:41:05 +0530

    CB setup scripts

---
+++ resources/ndc/couchbase/create-env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+curl -v -X PUT 'http://repo-svc-app-0001.nm.flipkart.com:8080/env/fk-connekt-couchbase?appkey=connekt'  -d \
+'[
+     {
+                        "repoName": "couchbase",
+                        "repoVersion": 1,
+                        "repoReferenceType": "HEAD"        
+    },
+    {
+                        "repoName": "libssl",
+                        "repoVersion": 1,
+                        "repoReferenceType": "HEAD"        
+    }
+]'   -H 'Content-Type: application/json'
