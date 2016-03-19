90891a5f37e601882ccdbf520b1a04739a6284d1
Kinshuk Bairagi <me@kinshuk.in>
2016-03-19 06:58:09 +0530

    update hbase make

--- resources/ndc/hbase/make.sh
+++ resources/ndc/hbase/make.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance create --appId  fk-connekt-hbase-nn --type i1.large --users /home/kinshuk.bairagi/users.txt --script
+kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance create --appId  fk-connekt-hbase --type d1.xlarge --users /home/kinshuk.bairagi/users.txt --script /home/aman.shrivastava/ambari/setup/setup.sh --tag dn-firestorm
+kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance create --appId  fk-connekt-hbase --type d1.medium --users /home/kinshuk.bairagi/users.txt --script /home/aman.shrivastava/ambari/setup/setup.sh --tag collector-firestorm
+kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance create --appId  fk-connekt-hbase --type i1.large --users /home/kinshuk.bairagi/users.txt --script /home/aman.shrivastava/ambari/setup/setup.sh --tag jn-firestorm
+kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance create --appId  fk-connekt-hbase --type c1.medium --users /home/kinshuk.bairagi/users.txt --script /home/aman.shrivastava/ambari/setup/setup.sh --tag ambari-firestorm
+kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance create --appId  fk-connekt-hbase --type i1.xlarge --users /home/kinshuk.bairagi/users.txt --script /home/aman.shrivastava/ambari/setup/setup.sh --tag nn-firestorm
 
-kloud-cli --endpoint 10.33.65.0   instance describe --appId fk-connekt-hbase-nn --instanceId 210496
+kloud-cli --endpoint 10.33.65.0   instance describe --appId fk-connekt-hbase --instanceId 397061
 
-#kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance destroy --appId fk-connekt-hbase-nn --instanceId 259750
+#kloud-cli --endpoint 10.33.65.0 --user connekt-dev  instance destroy --appId fk-connekt-hbase --instanceId 397067
 
-kloud-cli --endpoint 10.33.65.0   instance list --appId fk-connekt-hbase-nn
+kloud-cli --endpoint 10.33.65.0   instance list --appId fk-connekt-hbase
+
+
+#kill
+#kloud-cli --endpoint ga  --user connekt-dev  instance list  --appId fk-connekt-hbase | grep id | tr -td ":\" id" | xargs -I {} kloud-cli --endpoint ga --user connekt-dev  instance destroy --appId fk-connekt-hbase --instanceId {}
+
