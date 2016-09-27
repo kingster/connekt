31823ea2790e3e5b06747d818a4115317f7abb0d
Durga Sahoo <durga.s@flipkart.com>
2016-09-27 15:40:05 +0530

    CNKT-001 : remove all resources, move to connekt-toolbox

--- resources/ndc/smtp/make.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-kloud-cli --zone in-mumbai-gateway --user connekt-dev  instance create --appId fk-connekt-smtp --type c0.xlarge.e1 --users /home/kinshuk.bairagi/users.txt --script /home/kinshuk.bairagi/setup.sh --tag bcc
-
-#kloud-cli --zone in-mumbai-gateway --user connekt-dev  instance destroy --appId fk-connekt-smtp --instanceId 393258
-
-#kloud-cli --zone in-mumbai-gateway  --user connekt-dev instance describe --appId fk-connekt-smtp --instanceId 393258
-
-kloud-cli --zone in-mumbai-gateway  instance list  --appId fk-connekt-smtp
